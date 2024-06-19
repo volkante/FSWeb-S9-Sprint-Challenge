2ea049b186cae70f9d96a88f2386081a6be35a5f
volkantemelli <volkan.temelli@gmail.com>
2024-06-19 22:31:30 +0200

    done

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,49 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import "@testing-library/jest-dom";
+import AppFunctional from "./AppFunctional";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+beforeEach(() => {
+  render(<AppFunctional className="functional" />);
+});
+
+test("Koordinatlar başlığı doğru şekilde render ediliyor", () => {
+  const coordinateTitle = screen.getByTestId("coordinatesH3");
+  expect(coordinateTitle).toHaveTextContent("Koordinatlar");
+});
+
+test("email yazılınca inputtaki değer değişiyor", async () => {
+  const emailInput = screen.getByPlaceholderText("email girin");
+  await userEvent.type(emailInput, "ali@gmail.com");
+  expect(emailInput.value).toBe("ali@gmail.com");
+});
+
+test("sol buton calisiyor", async () => {
+  const leftBtn = screen.getByText(/SOL/i);
+  const coordinateTitle = screen.getByTestId("coordinatesH3");
+  await userEvent.click(leftBtn);
+  expect(coordinateTitle).toHaveTextContent("Koordinatlar (1,2)");
+});
+
+test("resete basınca üstteki sayac sıfırlanıyor", async () => {
+  const resetBtn = screen.getByText("reset");
+  const leftBtn = screen.getByText(/SOL/i);
+  const counterH3 = screen.getByTestId("stepsH3");
+
+  await userEvent.click(leftBtn);
+  await userEvent.click(resetBtn);
+  expect(counterH3).toHaveTextContent("0 kere ilerlediniz");
+});
+
+test("email dogru girilmisse submit ediliyor ve response h3'e işleniyor", async () => {
+  const emailInput = screen.getByPlaceholderText("email girin");
+  const submitBtn = screen.getByTestId("submitBtn");
+  const messageH3 = screen.getByTestId("messageH3");
+
+  await userEvent.type(emailInput, "ali@gmail.com");
+  await userEvent.click(submitBtn);
+
+  await waitFor(() => expect(messageH3).toHaveTextContent("win"));
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,14 +1,70 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
+//You can't go left
+//You can't go right
+//You can't go up
+//You can't go down
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+
+  const [email, setEmail] = useState(initialEmail);
+  const [activeSq, setActiveSq] = useState(initialIndex);
+  const [stepCount, setStepCount] = useState(initialSteps);
+  const [message, setMessage] = useState(initialMessage);
+
+  let x, y;
+  if (activeSq >= 0 && activeSq <= 2) {
+    x = activeSq + 1;
+    y = 1;
+  } else if (activeSq >= 3 && activeSq <= 5) {
+    x = activeSq - 2;
+    y = 2;
+  } else if (activeSq >= 6 && activeSq <= 8) {
+    x = activeSq - 5;
+    y = 3;
+  }
+
+  function activeHandler(evt) {
+    setMessage(initialMessage);
+    const { id } = evt.target;
+    if (id === "left") {
+      if (!(activeSq % 3 === 0)) {
+        setActiveSq(activeSq - 1);
+        setStepCount(stepCount + 1);
+      } else {
+        setMessage("Sola gidemezsiniz");
+      }
+    } else if (id === "right") {
+      if (!((activeSq + 1) % 3 === 0)) {
+        setActiveSq(activeSq + 1);
+        setStepCount(stepCount + 1);
+      } else {
+        setMessage("Sağa gidemezsiniz");
+      }
+    } else if (id === "up") {
+      if (!(activeSq < 3)) {
+        setActiveSq(activeSq - 3);
+        setStepCount(stepCount + 1);
+      } else {
+        setMessage("Yukarıya gidemezsiniz");
+      }
+    } else if (id === "down") {
+      if (!(activeSq > 5)) {
+        setActiveSq(activeSq + 3);
+        setStepCount(stepCount + 1);
+      } else {
+        setMessage("Aşağıya gidemezsiniz");
+      }
+    }
+  }
 
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
@@ -23,6 +79,10 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setActiveSq(initialIndex);
+    setStepCount(initialSteps);
+    setMessage(initialMessage);
+    setEmail(initialEmail);
   }
 
   function sonrakiIndex(yon) {
@@ -38,41 +98,84 @@ export default function AppFunctional(props) {
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    setEmail(evt.target.value);
   }
 
-  function onSubmit(evt) {
+  async function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: x,
+        y: y,
+        steps: stepCount,
+        email: email,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setMessage(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        setMessage(err.response.data.message);
+      });
+    setEmail(initialEmail);
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3
+          id="coordinates"
+          data-testid="coordinatesH3"
+        >{`Koordinatlar (${x},${y})`}</h3>
+        <h3 id="steps" data-testid="stepsH3">
+          {stepCount} kere ilerlediniz
+        </h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === activeSq ? " active" : ""}`}
+          >
+            {idx === activeSq ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message" data-testid="messageH3">
+          {message}
+        </h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={activeHandler}>
+          SOL
+        </button>
+        <button id="up" onClick={activeHandler}>
+          YUKARI
+        </button>
+        <button id="right" onClick={activeHandler}>
+          SAĞ
+        </button>
+        <button id="down" onClick={activeHandler}>
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
-        <input id="submit" type="submit"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          onChange={onChange}
+          value={email}
+          data-testid="email"
+        ></input>
+        <input id="submit" type="submit" data-testid="submitBtn"></input>
       </form>
     </div>
-  )
+  );
 }
